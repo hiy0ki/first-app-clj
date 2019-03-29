d548b04db60419b893dec58008118a7d0495cc5f
hiy0ki <hiyokix@gmail.com>
2019-03-29 18:37:43 +0900

    独自でrouting実装

--- src/first_web_app/core.clj
+++ src/first_web_app/core.clj
@@ -1,18 +1,81 @@
 (ns first-web-app.core
   (:require [ring.adapter.jetty :as server]))
 
-(defonce server (atom nil))
+(defn ok [body]
+  {:status 200
+   :body body})
+
+(defn html [res]
+  (assoc res :headers {"Content-Type" "text/html; charset=utf-8"}))
+
+(defn not-found []
+  {:status 404
+   :body "<h1>404 page not found</h1>"})
+
+(defn home-view [req]
+  "<h1>ホーム画面</h1>
+   <a href=\"/todo\">TODO 一覧</a>")
+
+(defn home [req]
+  (-> (home-view req)
+      ok
+      html))
+
+;; ↑のhomeは以下と同じ
+;(defn home [req]
+;  (html (ok (home-view req))))
+
+
+(def todo-list
+  [{:title "朝ゴハンを作る"}
+   {:title "朝ゴハンを食べる"}
+   {:title "朝ゴハンを燃やす"}
+   {:title "朝ゴハンを片付ける"}])
+
+(defn todo-index-view [req]
+  `("<h1>TODO 一覧</h1>"
+    "<ul>"
+    ~@(for [{:keys [title]} todo-list]
+        (str "<li>" title "</li>"))
+    "</ul>"))
+
+(defn todo-index [req]
+  (-> (todo-index-view req)
+      ok
+      html))
+   
+(def routes
+  {"/" home
+   "/todo" todo-index})
+
+
+(defn match-route [uri]
+  (get routes uri))
 
 (defn handler [req]
-  {:status 200
-   :headers {"Content-type" "text/html"}
-   :body "<h1>hello, world!!aa</h1>"})
+  (let [uri (:uri req)
+        maybe-fn (match-route uri)]
+    (if maybe-fn
+      (maybe-fn req)
+      (not-found))))
+
+
+(defonce server (atom nil))
+
+;(defn handler [req]
+;  {:status 200
+;   :headers {"Content-type" "text/html"}
+;   :body "<h1>hello, world!!</h1>"})
 
 (defn start-server [& {:keys [host port join?]
                        :or {host "localhost" port 3000 join? false}}]
   (let [port (if (string? port) (Integer/parseInt port) port)]
     (when-not @server
-      (reset! server (server/run-jetty #'handler {:host host :port port :join? join?})))))
+      (reset! server (server/run-jetty
+                      #'handler
+                      {:host host
+                       :port port
+                       :join? join?})))))
 
 (defn stop-server []
   (when @server
@@ -23,9 +86,4 @@
   (when @server
     (stop-server)
     (start-server)))
-
-
-;(def routes
-;  {"/" home
-;   "/todo" todo-index})
 
